6f328c5c1d84dfcbadecaf2d9e02822490200c4d
Your Name <you@example.com>
2025-08-22 09:47:14 +0000

    created all these files of react

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+/** @type {import('tailwindcss').Config} 
+export default {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}", // Include .js and .jsx files for complete detection
+/*  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+*/
