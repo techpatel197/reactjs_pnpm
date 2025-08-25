350b2e2b8602b091a96283cca6c8f7f22375c8ed
techpatel197 <techpatel197@gmail.com>
2025-08-25 18:14:40 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Index() {
-  return <div>Welcome to GetFloora-World ! Preparing Email</div>;
+  return <div>Welcome to GetFloora-World ! Preparing Email ! Hello-Dev</div>;
 }
